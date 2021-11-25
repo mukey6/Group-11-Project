3af6100b6bbb0ca32815d3e6084f924b00ba9cde
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-11-24 18:32:44 -0600

    final touch, clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-// b49c94e39bb5414b2d07ad05bd3ec5f2	
+var recipeApiKey = "b49c94e39bb5414b2d07ad05bd3ec5f2"	
+let imageApiKey ="RUQuxPjGR6Ilkox7XXVJeDgtrx5S3OXASTUxQRUKq3w"
 var searchButton = document.querySelector("#search-btn")
 var result = document.querySelector("#result")
 let resultContainer = document.getElementById("result-container")
@@ -7,36 +8,30 @@ let imageTwo = document.getElementById("image-2")
 
 function searchRecipe(){
 let searchInput=$("#search-input").val()  
-  console.log(searchInput)
 
+    var apiRecipe = "https://api.edamam.com/api/recipes/v2?type=public&q="+searchInput+"&app_id=a2a764d9&app_key=" + recipeApiKey
 
-    var apir = "https://api.edamam.com/api/recipes/v2?type=public&q="+searchInput+"&app_id=a2a764d9&app_key=b49c94e39bb5414b2d07ad05bd3ec5f2"
-    console.log(apir)
-    
-    fetch(apir).then(function(response){
+    fetch(apiRecipe).then(function(response){
         if (response.ok){
             return response.json()
         } 
     })
     .then(function(data){
-        console.log("1st data ", data)
+
         searchedResult(data)
     })
-    
-    var imageApi = "https://api.unsplash.com/search/photos?page=1&query="+searchInput+"&client_id=RUQuxPjGR6Ilkox7XXVJeDgtrx5S3OXASTUxQRUKq3w"
-    console.log(imageApi)
+    var imageApi = "https://api.unsplash.com/search/photos?page=1&query="+searchInput+"&client_id=" + imageApiKey
+
     fetch(imageApi).then(function(response){
         if(response.ok){
             return response.json()
         }
     })
     .then(function(data2){
-        console.log('2nd data', data2);
         searchedImages(data2)
 
     })
 }
-
 function searchedResult(data){
     resultContainer.innerHTML=""
 let title = document.createElement("h4")
@@ -62,9 +57,8 @@ source.innerText = "Source: " + data.hits[0].recipe.source
 resultContainer.appendChild(source)
 var aTag = document.createElement('a')
 aTag.setAttribute('href', data.hits[0].recipe.url)
-aTag.innerHTML = data.hits[3].recipe.url
+aTag.innerHTML = data.hits[0].recipe.url
 resultContainer.appendChild(aTag)
-
 
 let titleTwo = document.createElement("h4")
 titleTwo.innerText = data.hits[3].recipe.label
@@ -82,7 +76,6 @@ for (let i = 0; i < list.length; i++) {
 
     ingredientLine.appendChild(ingredientLists)
     ingredientLists.innerText= list[i]
-
 }
 
 let sources = document.createElement("p")
@@ -98,13 +91,9 @@ function searchedImages(data2){
 firstImage = data2.results[0].urls.small
 imageOne.innerHTML=("<img src='" + firstImage  + "'>")
 secondImage = data2.results[1].urls.small
-console.log(imageOne )
 
 imageTwo.innerHTML=("<img src='" + secondImage  + "'>")
-console.log(imageTwo)
 
 }
 searchButton.addEventListener("click",searchRecipe )
 
-// RUQuxPjGR6Ilkox7XXVJeDgtrx5S3OXASTUxQRUKq3w
-// 
